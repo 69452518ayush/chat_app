1d764c1b6abb5cbffede8e4ff6497722e662ab4f
ayush <ayushpandey.pratapgarh@gmail.com>
2026-01-21 16:21:42 +0530

    A full Fledge Chat App built with flutter and Firebase and add

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,1 +1,17 @@
+const logo = "assets/Illustration (1).png";
+const frame = "assets/splash_frame.png";
 
+const splash = "/splash";
+
+// Icons
+const chatsIcon = "assets/message.png";
+const homeIcon = "assets/home.png";
+const profileIcon = "assets/profile1.png";
+const searchIcon = "assets/Search.png";
+
+// Auth
+
+const signup = "/signup";
+const login = "/login";
+const wrapper = "/wrapper";
+const chatRoom = "/chatRoom";
